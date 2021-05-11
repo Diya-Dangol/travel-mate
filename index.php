2d905f8f1fb1b3b4b41acc55b7a90a8e7312853e
Diya-Dangol <diya.dangol15@gmail.com>
2021-05-11 18:06:47 +0545

    about us section, card and subscribe banner

--- index.php
+++ index.php
@@ -75,22 +75,6 @@
                         <input value="500" min="500" max="50000" step="500" type="range">
                         <input value="50000" min="500" max="50000" step="500" type="range">
                     </section>
-                   <!-- <div id="time-range">
-                        <span class="slider-time">9</span> - <span class="slider-time2">5</span>
-
-                        </p>
-                        <div class="sliders_step1">
-                            <div id="slider-range"></div>
-                        </div>
-                    </div>-->
-
-
-                    
-                    <!--
-                    <label>Price range</label>
-                    <br/>
-                    <text>399-5000</text>
-                    <input type="range" min="1" max="100" value="50" class="slider" id="myRange">-->
                 </div>
 
 
@@ -256,8 +240,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -310,8 +294,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -364,8 +348,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -418,8 +402,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -473,8 +457,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -528,8 +512,8 @@
                             <text>Adventurous</text>
                         </div>
                     </article>
-                    <article class="price-wrap">
-                        <p>Rs.8000 <span class="price">/per person</span> </p>
+                    <article class="button-wrap">
+                        <p id="price">Rs.8000 <span class="per-person">/per person</span> </p>
                         <button id="button" class="pushable" >
                             <span class="front">
                                 <a href="nepal.php">Book now</a>
@@ -548,47 +532,171 @@
     </div>
 
     <!-- card banner -->
-    <div class="banner">
-        <img src="assets/vacations.jpg" style="width:100%; height: 300px;">
+    <div class="banner-card">
+        <img src="assets/vacations.jpg" width="100%" height="350px">
         <div class="heading">
             <h2 class="title">
                 we accept
                 <hr>
             </h2>
         </div>
+        <article class="icon">
+            <i class="fa fa-cc-visa"></i>
+            <i class="fa fa-cc-mastercard"></i>
+            <i class="fa fa-cc-paypal"></i>
+            <i class="fa fa-credit-card"></i>
+            <i class="fa fa-cc-discover"></i>
+        </article>
     </div>
 
     <!-- about us -->
-    <div id="container">
+    <div id="container-grey">
         <div class="heading">
             <h2>about us
                 <hr>
                 <p>lets introduce ourselves</p>
             </h2>
         </div>
-
-        <section class="row">
-            <article class="column">
-                <article class="card"> 
-                    <img src="assets/kathmandu.jpg" alt=""  width="350px" height="450px">
-                    <i class="fa fa-binoculars"></i>
-                    <p>
-                        nepal 
-                       <span>05 palces</span>
-                    </p>
-                </article>
+        <!--  our story  -->
+        <div class="container-about">       
+            <div class="sub-heading">
+                <h3>our story
+                    <hr>
+                </h3>
+            </div>
+            <article class="story">
+                <img src="assets/kathmandu.jpg" alt=""  width="500px" height="370px">
+                <article class="content">
+                    <article class="paragraph">
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                        <p>
+                            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum 
+                        </p>
+                    </article>
+                    <article class="button-wrap">
+                        <button id="button" class="pushable" >
+                            <span class="front">
+                                <a href="about.php">read more <i class="fa fa-arrow-right"></i></a>
+                            </span>
+                        </button>
+                    </article>
+                </article> 
             </article>
-        </section>
+        </div>
+        <!--  our services  -->
+        <div class="container-about">       
+            <div class="sub-heading">
+                <h3>our services
+                    <hr>
+                </h3>
+            </div>
+            <article class= "row">
+                <article class="column">
+                    <article class='icon'>
+                        <i class="fa fa-hotel"></i>
+                    </article>
+                    <article class="content">
+                        <div class="sub-heading">
+                            <h3>hotel accomodation</h3>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>                        
+                    </article> 
+                </article>
+                <article class="column">
+                    <article class='icon'>
+                        <i class="fa fa-hotel"></i>
+                    </article>
+                    <article class="content">
+                        <div class="sub-heading">
+                            <h3>delicious food</h3>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>                        
+                    </article> 
+                </article>
+                <article class="column">
+                    <article class='icon'>
+                        <i class="fa fa-hotel"></i>
+                    </article>
+                    <article class="content">
+                        <div class="sub-heading">
+                            <h3>luxury travel</h3>
+                        </div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        </p>                        
+                    </article> 
+                </article>
+              
+            </article>
+        </div>
+        <!-- our partner -->
+        <div class="container-about">       
+            <div class="sub-heading">
+                <h3>our partner
+                    <hr>
+                </h3>
+            </div>
+            <article class= "partner-row">
+                <article class="column">
+                    <article class="content">
+                        <img src="assets/drop.png" height="60px" width="60px" >
+                        <p>hotel accomodation</p>
+                    </article> 
+                </article>
+                <article class="column">
+                    <article class="content">
+                        <img src="assets/drop.png" height="60px" width="60px" >
+                        <p>hotel accomodation</p>
+                    </article> 
+                </article>
+                <article class="column">
+                    <article class="content">
+                        <img src="assets/drop.png" height="60px" width="60px" >
+                        <p>hotel accomodation</p>
+                    </article> 
+                </article>
+                <article class="column">
+                    <article class="content">
+                        <img src="assets/drop.png" height="60px" width="60px" >
+                        <p>hotel accomodation</p>
+                    </article> 
+                </article>
+              
+               
+              
+            </article>
+        </div>
     </div>
 
-    <!-- card banner -->
-    <div class="banner">
-        <img src="assets/vacations.jpg" style="width:100%; height: 300px;">
-        <div class="heading">
+    <!-- subscribe banner -->
+    <div class="banner-subscribe">
+        <img src="assets/sunset.jpg" style="width:100%; height: 350px;">
+        <div class="subscribe-content">
             <h2 class="title">
-                we accept
+                subscribe our newsletter
                 <hr>
+                <p id="subtitle">Lorem ipsum text about why you should subscribe to our newsletter blabla. Lorem ipsum text about why you should subscribe to our newsletter blabla</p>
             </h2>
+            <form action="contact.php">
+                <div class="form-group">
+                    <input type="email" class="form-control" id="email" name="email" placeholder="Email" value="email">
+                </div>
+
+                <button id="button" class="pushable" >
+                    <span class="front">
+                        <a href="trip-list.php" id="front" >subscribe <i class='fa fa-subscribe'></i> </a>
+                    </span>
+                </button>
+            </form>
         </div>
     </div>
    
